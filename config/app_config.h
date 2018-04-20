2cc1f2604762085279a5a75941f7fb8f401a4dd0
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-04-21 07:23:57 +0900

    最初のコミット

---
+++ config/app_config.h
@@ -0,0 +1,24 @@
+/**
+ * File:  app_config.h
+ * Author: ymiyamoto
+ *
+ * Created on Sat Apr 21 06:30:54 2018
+ */
+
+#ifndef _APP_CONFIG_H_
+#define _APP_CONFIG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define APP_TIMER_ENABLED 1
+#define NRF_SDH_ENABLED 1
+#define NRF_SDH_BLE_ENABLED 1
+#define NRF_PWR_MGMT_ENABLED 1
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _APP_CONFIG_H_ */
